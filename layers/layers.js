796de49ba105452c9aa85983e7e4fad5440690b6
lucazfontenele <78514636+lucazfontenele@users.noreply.github.com>
2022-02-15 16:54:03 -0300

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,77 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleRoad_0 = new ol.layer.Tile({
+            'title': 'Google Road',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.google.at/permissions/geoguidelines/attr-guide.html">Map data ©2015 Google</a>',
+                url: 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+            })
+        });
+
+        var lyr_WikimediaMap_1 = new ol.layer.Tile({
+            'title': 'Wikimedia Map',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_BingMap_2 = new ol.layer.Tile({
+            'title': 'Bing Map',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'http://ecn.dynamic.t0.tiles.virtualearth.net/comp/CompositionHandler/{q}?mkt=en-us&it=G,VE,BX,L,LA&shading=hill'
+            })
+        });
+
+        var lyr_OSMStandard_3 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_GooglecnNormal_4 = new ol.layer.Tile({
+            'title': 'Google.cn Normal',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.google.com/intl/zh-CN_cn/permissions/geoguidelines/attr-guide.html">地图数据 ©2016 Google</a>',
+                url: 'http://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}'
+            })
+        });
+
+        var lyr_WazeWorld_5 = new ol.layer.Tile({
+            'title': 'Waze (World)',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://worldtiles3.waze.com/tiles/{z}/{x}/{y}.png'
+            })
+        });
+
+lyr_GoogleRoad_0.setVisible(true);lyr_WikimediaMap_1.setVisible(true);lyr_BingMap_2.setVisible(true);lyr_OSMStandard_3.setVisible(true);lyr_GooglecnNormal_4.setVisible(true);lyr_WazeWorld_5.setVisible(true);
+var layersList = [lyr_GoogleRoad_0,lyr_WikimediaMap_1,lyr_BingMap_2,lyr_OSMStandard_3,lyr_GooglecnNormal_4,lyr_WazeWorld_5];
